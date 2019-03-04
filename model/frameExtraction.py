c0e2d5a126bd5ec7b7e77bfe1c75a0f688b6679b
Claudia Baca Perez <claudiabaca.perez@e-campus.uab.net>
2019-03-04 13:49:01 +0100

    frames1

--- model/frameExtraction.py
+++ model/frameExtraction.py
@@ -43,8 +43,8 @@ folder_frame ='/Users/claudiabacaperez/Desktop/mcv-m6-2019-team2/datasets/train/
 
 dir_gt='/Users/claudiabacaperez/Desktop/mcv-m6-2019-team2/datasets/train/S03/c010/det/det_yolo3.txt'
 dir_gt='/Users/claudiabacaperez/Desktop/mcv-m6-2019-team2/datasets/train/S03/c010/det/det_yolo3.txt'
-#frame_extraction_ffmpeg(source, folder_frame)
-
+frame_extraction_ffmpeg(source, folder_frame)
+"""
 #getgroundTruth(dir_gt)
 
 bb=BoundingBoxes_Video()
@@ -58,7 +58,7 @@ bb2.getgroundTruth(dir_gt)
 #print(len(bb2.listGd))
 
 list=iou_overtime(bb,bb2)
-print(len(list))
+print(len(list))"""
 """[TP,FP,FN]=iou_gt(bb2,bb)
 [precision, sensitivity, accuracy]= performance_evaluation(TP, FN, FP)
 print(TP)
