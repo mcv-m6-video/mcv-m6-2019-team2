646a73359d6cc095ffcbc1d210739bb2c09eeb98
Quim Comas Martínez <joaquim.comas@e-campus.uab.cat>
2019-03-05 01:16:00 +0100

    read xml files and converted to ai city format

---
+++ model/read_annotation.py
@@ -0,0 +1,53 @@
+import xml.etree.cElementTree as ET
+
+import numpy as np
+
+
+def read_annotations(file):
+    root = ET.ElementTree(file=file).getroot()
+
+    tracks = root.findall('.//track')
+
+    annot = []
+    frame = []
+    xt = []
+    yt = []
+    width = []
+    heigth = []
+
+    for track in tracks:
+
+        label = track.attrib['label']
+
+        if label == 'car':
+
+            boxes = track.findall('./box')
+
+            for box in boxes:
+                frameid = int(box.get('frame'))
+                frame.append(frameid)
+                xtl = float(box.get('xtl'))
+                xt.append(xtl)
+                ytl = float(box.get('ytl'))
+                yt.append(ytl)
+                xbr = float(box.get('xbr'))
+                width.append(xbr - xtl + 1)
+                ybr = float(box.get('ybr'))
+                heigth.append(ybr - ytl + 1)
+
+    id = np.negative(np.ones(len(frame)))
+
+    conf = np.ones(len(frame))
+
+    # Ai city Format [frame, -1, left, top, width, height, conf, -1, -1, -1]
+
+    annot = np.column_stack((frame, id, xt, yt, width, heigth, conf, id, id, id))
+
+    np.savetxt('annot', annot, delimiter=',', fmt='% 4d')
+
+    return annot
+
+
+file = "/Users/quim/Desktop/untitled/annotations/AI_CITY_S03_C01_391_764.xml"
+
+read_annotations(file)
